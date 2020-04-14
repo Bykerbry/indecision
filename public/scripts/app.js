28d660f11d419328905f6c4fccaeabbdf13c425c
Bryce <bykerbry@umich.edu>
2020-04-13 23:59:37 -0400

    dynamic data display

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,50 @@
 'use strict';
 
+var app = {
+    title: 'Indecision',
+    subtitle: 'Some app content...'
+};
+
 var template = React.createElement(
-  'p',
-  null,
-  'This is JSX!!!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    )
+);
+var me = {
+    name: 'Bryce',
+    age: 27,
+    location: 'Grand Rapids, MI'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        me.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        me.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        me.location
+    )
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,4 +1,30 @@
-const template = <p>This is JSX!!!</p>
+
+
+const app = {
+    title: 'Indecision',
+    subtitle: 'Some app content...'
+}
+
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        
+    </div>
+)
+const me = {
+    name: 'Bryce',
+    age: 27,
+    location: 'Grand Rapids, MI'
+}
+
+const templateTwo = (
+    <div>
+        <h1>{me.name}</h1>
+        <p>Age: {me.age}</p>
+        <p>Location: {me.location}</p>
+    </div>
+)
 const appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
