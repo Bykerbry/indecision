5f9cd07cc26f504f1fef2c3a5faa87dc99e0f767
Bryce <bykerbry@umich.edu>
2020-04-14 23:33:44 -0400

    Options can now be added, displayed & randomly selected

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,50 +2,137 @@
 
 var app = {
     title: 'Indecision',
-    subtitle: 'Some app content...'
+    subtitle: 'Some app content...',
+    options: []
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.subtitle
-    )
-);
-var me = {
-    name: 'Bryce',
-    age: 27,
-    location: 'Grand Rapids, MI'
+var decision = '';
+
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+    render();
 };
 
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        me.name
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Age: ',
-        me.age
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        me.location
-    )
-);
-var appRoot = document.getElementById('app');
+var removeAll = function removeAll() {
+    app.options = [];
+    render();
+};
 
-ReactDOM.render(template, appRoot);
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    decision = app.options[randomNum];
+    render();
+};
+
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options[0] ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    'li',
+                    { key: option },
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            'button',
+            { disabled: !app.options[0], onClick: onMakeDecision },
+            'What to do?'
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        ),
+        decision && React.createElement(
+            'p',
+            null,
+            decision
+        )
+    );
+
+    var appRoot = document.getElementById('app');
+    ReactDOM.render(template, appRoot);
+};
+
+render();
+
+/*
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getDesc() {
+        return `${this.name} is ${this.age} years old.`
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age)
+        this.major = major;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, home) {
+        super(name, age)
+        this.home = home;
+    }
+    getDesc() {
+        return super.getDesc() + ` I'm visiting from ${this.home}`
+    }
+}
+
+const me = new Person('Bryce', 27)
+console.log(me.getDesc());
+
+const b = new Student('Bryce', 28, 'Math')
+console.log(b.getDesc());
+
+const t = new Traveler('Fran', 26, 'Grand Rapids')
+console.log(t.getDesc());
+
+*/
--- src/app.js
+++ src/app.js
@@ -1,30 +1,97 @@
 
-
 const app = {
     title: 'Indecision',
-    subtitle: 'Some app content...'
+    subtitle: 'Some app content...',
+    options: []
 }
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
-        
-    </div>
-)
-const me = {
-    name: 'Bryce',
-    age: 27,
-    location: 'Grand Rapids, MI'
+let decision = ''
+
+const onFormSubmit = (e) => {
+    e.preventDefault()
+    const option = e.target.elements.option.value;
+
+    if(option) {
+        app.options.push(option)
+        e.target.elements.option.value = ''
+    }
+    render()
 }
 
-const templateTwo = (
-    <div>
-        <h1>{me.name}</h1>
-        <p>Age: {me.age}</p>
-        <p>Location: {me.location}</p>
-    </div>
-)
-const appRoot = document.getElementById('app')
+const removeAll = () => {
+    app.options = []
+    render()
+}
 
-ReactDOM.render(template, appRoot)
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length)
+    decision = app.options[randomNum];
+    render()
+}
+
+const render = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options[0] ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                { app.options.map(option => <li key={option}>{option}</li>) }            
+            </ol>
+            <button disabled={!app.options[0]} onClick={onMakeDecision}>What to do?</button>
+            <button onClick={removeAll}>Remove All</button>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+            {decision && <p>{decision}</p>}
+        </div>
+    )
+    
+    const appRoot = document.getElementById('app')
+    ReactDOM.render(template, appRoot)        
+}
+
+render()
+
+
+
+/*
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getDesc() {
+        return `${this.name} is ${this.age} years old.`
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age)
+        this.major = major;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, home) {
+        super(name, age)
+        this.home = home;
+    }
+    getDesc() {
+        return super.getDesc() + ` I'm visiting from ${this.home}`
+    }
+}
+
+const me = new Person('Bryce', 27)
+console.log(me.getDesc());
+
+const b = new Student('Bryce', 28, 'Math')
+console.log(b.getDesc());
+
+const t = new Traveler('Fran', 26, 'Grand Rapids')
+console.log(t.getDesc());
+
+*/
